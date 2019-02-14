514c07c5adef4db079dbdc7a1a30454977d263fc
Pieter Provoost <pieterprovoost@gmail.com>
2019-02-14 14:14:11 +0100

    first commit

---
+++ script.R
@@ -0,0 +1,24 @@
+library(dplyr)
+library(jsonlite)
+
+areas <- fromJSON("https://api.obis.org/area")$results %>%
+  filter(type == "iho") %>%
+  mutate(id = as.numeric(id)) %>%
+  select(areaid = id, areaname = name)
+
+sp <- read.csv("woa_2.csv")
+
+stats <- sp %>%
+  group_by(speciesid, subphylumid, areaid, depth) %>%
+  summarize(records = n()) %>%
+  arrange(areaid, depth, desc(records)) %>%
+  filter(is.na(subphylumid) | subphylumid != 146419) %>%
+  left_join(areas, by = "areaid")
+
+write.csv(stats, file = "stats.csv", row.names = FALSE, na = "")
+
+areastats <- stats %>%
+  group_by(areaname, depth) %>%
+  summarize(records = sum(records), species = length(unique(speciesid)))
+
+write.csv(areastats, file = "areastats.csv", row.names = FALSE, na = "")
